1d8b30221d7bf22a7826144c64f8f13585aead82
XavierMerino01 <71768212+XavierMerino01@users.noreply.github.com>
2025-02-06 11:19:38 +0100

    Project card centered + Footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear()} Xavier Merino Seuma.</p>
             <div>
-                <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">GitHub</a>
-                <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">Twitter</a>
+                <a href="https://github.com/XavierMerino01" target="_blank" rel="noopener noreferrer">GitHub</a>
+                <a> </a>
+                <a href="https://linkedin.com/in/xaviermerino" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             </div>
         </footer>
     );
