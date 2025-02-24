fe152b753135b5b1bf6ada84990458ca1b364cb8
XavierMerino01 <merinoxevi@gmail.com>
2025-02-24 16:36:47 +0100

    Updates

--- main.js
+++ main.js
@@ -961,7 +961,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "/";
+/******/ 		__webpack_require__.p = "/portfolio/";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
